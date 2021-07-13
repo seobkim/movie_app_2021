bb816bbff6292ec793cbf4f63fdab1f05cf10627
seobkim <kks7824@gmail.com>
2021-07-13 17:54:36 +0900

    94Page success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+function Food(props){
+  return (<div>
+      <h2>I Like {props.name}</h2>
+      <img src={props.picture}/>
+    </div>)
+    ;
 
+}
+const foodILike=[
+  {
+    name:'Kimchi',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tY7yWP7-KjRrTilhGr6ZggHaE9%26pid%3DApi&f=1'
+  },
+  {
+    name:'Samgyeopsal',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.v0RiALj-qb0tA3TaocYnVAHaE7%26pid%3DApi&f=1'
+  },
+  {
+    name:'Bibimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EbPzuedGhvUxXy7ufrwwAAHaE8%26pid%3DApi&f=1'
+  },
+  {
+    name:'Doncasu',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.17X5iMJQXahTyhVOQ3nlfQHaEK%26pid%3DApi&f=1'
+  },
+  {
+    name:'Kimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.K0ZVBZ76zesw6mZV3q3OcAHaFj%26pid%3DApi&f=1'
+  }
+
+];
+
+/* 구조 분해 할당 ES6문법 중 하나 
+  위와 같이 점 연산자를 사용하지않아도됨
+function Food({fav}){
+  console.log({fav})
+  return <h1>I Like {fav}</h1>
+}
+ */
+
+function renderFood(dish){
+  return <Food name = {dish.name} picture={dish.image}/>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; //App component import
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//(app_component,추가할 div 태그 네임)
+ReactDOM.render(<App />,document.getElementById('root'));
