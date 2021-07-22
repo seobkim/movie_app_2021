172a0f8a51f38f6c45d2ecb25796a9c6d4231a52
seobkim <kks7824@gmail.com>
2021-07-22 14:46:19 +0900

    120P 완료

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ class App extends React.Component{
     count : 0,
     
   };
+
+  add = () =>{
+    console.log('add');
+  }
+  minus = () =>{
+    console.log('minus');
+  }
+
   render(){
-    return <h1>I'm a class component</h1>
+    return( 
+    <div>
+    <h1>The number is :{this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>Minus</button>
+    </div>
+    )
   };
 }
 
