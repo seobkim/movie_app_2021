08fb35536d25ca89f5210c802e918f3d5bd673ed
seobkim <kks7824@gmail.com>
2021-09-23 21:26:19 +0900

    132p

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
   state={
     count : 0,
     
@@ -22,6 +27,7 @@ class App extends React.Component{
   }
 
   render(){
+    console.log('render');
     return( 
     <div>
     <h1>The number is :{this.state.count}</h1>
