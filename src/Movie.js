7cd30cc05cae8b962153a64e0fc30ea341636bc2
seobkim <kks7824@gmail.com>
2022-02-10 15:56:12 +0900

    200p modify css_file

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,17 @@ import propTypes from "prop-types";
 import React from "react";
 import "./Movie.css";
 
-function Movie({title,year,summary,poster}){
+function Movie({title,year,summary,poster,genres}){
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title}></img>
             <div className ="movie__data">
                 <h3 className = "movie__title">{title}</h3>
                 <h5 className = "movie__year">{year}</h5>
+                <ul className="movie_genres">
+                    {genres.map((genre,index) => {return <li key= {index} className="movie__genre">{genre}</li>
+                    })}
+                </ul>
                 <p className = "movie__summary">{summary}</p>
             </div>
         </div>
