2570329a7061844967aecfeb753301c40c28784c
seobkim <kks7824@gmail.com>
2022-02-15 19:07:42 +0900

    complete project

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,25 @@
 import propTypes from "prop-types";
 import React from "react";
 import "./Movie.css";
+import {Link} from 'react-router-dom';
 
 function Movie({title,year,summary,poster,genres}){
     return (
         <div className="movie">
-            <img src={poster} alt={title} title={title}></img>
-            <div className ="movie__data">
-                <h3 className = "movie__title">{title}</h3>
-                <h5 className = "movie__year">{year}</h5>
-                <ul className="movie_genres">
-                    {genres.map((genre,index) => {return <li key= {index} className="movie__genre">{genre}</li>
-                    })}
-                </ul>
-                <p className = "movie__summary">{summary.slice(0,180)}...</p>
-            </div>
+            <Link to={{pathname: 'movie-detail', state: {year,title,summary,poster,genres},
+            }}
+            >
+                <img src={poster} alt={title} title={title}></img>
+                <div className ="movie__data">
+                    <h3 className = "movie__title">{title}</h3>
+                    <h5 className = "movie__year">{year}</h5>
+                    <ul className="movie_genres">
+                        {genres.map((genre,index) => {return <li key= {index} className="movie__genre">{genre}</li>
+                        })}
+                    </ul>
+                    <p className = "movie__summary">{summary.slice(0,180)}...</p>
+                </div>
+            </Link>
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,15 @@
 import react from "react";
-
+import {Link} from 'react-router-dom';
+import './Navigation.css';
 function Navigation(){
     return(
-        <div>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+        <div className="nav">
+            <Link to="/">Home</Link>
+            
+            { /* pathname 은 url 의미 state는 route props 에 전할 데이터를 의미 */ }
+            {/*<Link to={{pathname:'./about',state:{fromNaviagation: true}}}>About</Link>*/}
+
+            <Link to='/about'>About</Link>
         </div>
     );
 }
