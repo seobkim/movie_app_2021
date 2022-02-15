47dc79879b38fbde33fde9b0e533527cc49d8b33
seobkim <kks7824@gmail.com>
2022-02-15 19:11:13 +0900

    fixed error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,10 @@
-import react from "react";
 import {Link} from 'react-router-dom';
 import './Navigation.css';
 function Navigation(){
     return(
         <div className="nav">
             <Link to="/">Home</Link>
-            
+
             { /* pathname 은 url 의미 state는 route props 에 전할 데이터를 의미 */ }
             {/*<Link to={{pathname:'./about',state:{fromNaviagation: true}}}>About</Link>*/}
 
